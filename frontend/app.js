e31a2ee11ed56f63e2704010cd61cbf85a3e6fc7
dimko33-lang <dimko33@gmail.com>
2026-04-07 09:39:04 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,17 @@
 
+async function sendMessage() {
+  const input = document.getElementById('input')
+  const message = input.value
+
+  const res = await fetch('http://localhost:3000/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message })
+  })
+
+  const data = await res.json()
+
+  console.log(data)
+}
